e605e9ea4d93391f1d905606288da8818387b16b
Le Hoang Anh Trung <trung.lha176892@sis.hust.edu.vn>
2020-04-09 15:33:11 +0700

    lab1

---
+++ lab1/FirstDialog.java
@@ -0,0 +1,9 @@
+//EX2: First Dialog.java
+import javax.swing.JOptionPane;
+public class FirstDialog{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null,"Hello World! How are you?");
+        System.exit(0);
+    }
+}
